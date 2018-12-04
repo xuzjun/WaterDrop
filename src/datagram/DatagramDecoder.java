2fea625f87636df1f69f3a1563e34d0d840f2993
xuzhojun <xuzhojun@163.com>
2018-12-04 17:38:46 +0800

    增加fastjson库

--- src/datagram/DatagramDecoder.java
+++ src/datagram/DatagramDecoder.java
@@ -7,6 +7,7 @@ import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.util.Map;
 
 /**
  * @author xuzjun
@@ -14,7 +15,6 @@ import java.nio.charset.CharsetDecoder;
 public class DatagramDecoder {
 
     public Datagram decode(ByteBuffer buffer) {
-        // buffer.flip();
         Charset charset = Charset.forName("gbk");
         CharsetDecoder decoder = charset.newDecoder();
         CharBuffer charBuffer = null;
@@ -25,6 +25,13 @@ public class DatagramDecoder {
         }
         String s = charBuffer.toString();
         System.out.println(s);
+        Map m = JSON.parseObject(s);
+        Object o = m.get("cmd");
+        Integer i = o == null ? null : o instanceof Integer ? (Integer) o : null;
+        if (i == null) {
+            return null;
+        }
+        Class class = DatagramFactory.getClass(i);
         Datagram datagramObject = JSON.parseObject(s, TraderLoginDatagram.class);
         return datagramObject;
     }
--- src/main/MultiThreadsMain.java
+++ src/main/MultiThreadsMain.java
@@ -14,6 +14,7 @@ public class MultiThreadsMain {
     public static void main(String[] args) {
         int port = Integer.valueOf(args[0]);
         System.out.println("port: " + port);
+
         new Thread(new Server(port, TransferStation.receiveQueue, TransferStation.sendQueue)).start();
 
         new Thread(new ProcessThread(TransferStation.receiveQueue, TransferStation.sendQueue, new DatagramDecoder(), new DatagramEncoder())).start();
